941f2b51421be1430c1849df0c9e60e38738fd50
Free Beachler <fbeachler@gmail.com>
2012-08-05 00:29:33 -0600

    added build profiles to ANT build; created assembly template for auto-generation by ANT; added 'zip-dist-files' ANT target; ANT build tested and working for 4 parts supported by this project's SCAD;

---
+++ tools/build/build_all_parts.sh
@@ -0,0 +1,4 @@
+ant build-filtered-assembly-scad -DprofileId=idler-acetal
+ant build-filtered-assembly-scad -DprofileId=motormount-acetal
+ant build-filtered-assembly-scad -DprofileId=idler-lm8uu
+ant build-filtered-assembly-scad -DprofileId=motormount-lm8uu
