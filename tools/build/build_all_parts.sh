27d7aae166671b74acca1b2d6596963533f60f97
Free Beachler <fbeachler@gmail.com>
2012-08-20 15:45:24 -0600

    added profile for no z-stop part sets and added part sets to /dist;

--- tools/build/build_all_parts.sh
+++ tools/build/build_all_parts.sh
@@ -2,3 +2,7 @@ ant build-filtered-assembly-scad -DprofileId=idler-acetal
 ant build-filtered-assembly-scad -DprofileId=motormount-acetal
 ant build-filtered-assembly-scad -DprofileId=idler-lm8uu
 ant build-filtered-assembly-scad -DprofileId=motormount-lm8uu
+ant build-filtered-assembly-scad -DprofileId=idler-acetal-nozstop
+ant build-filtered-assembly-scad -DprofileId=motormount-acetal-nozstop
+ant build-filtered-assembly-scad -DprofileId=idler-lm8uu-nozstop
+ant build-filtered-assembly-scad -DprofileId=motormount-lm8uu-nozstop
